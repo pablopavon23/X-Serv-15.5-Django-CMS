450b9cfefc2219bbe53476292d4a53d0531d4737
Pablo Pavon Pavon <ppavon@alpha26.aulas.gsyc.urjc.es>
2017-03-27 15:52:07 +0200

    Urls modificado

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from cms import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.barra, name='barra'),
+    url(r'^pagina/(\d+)$', views.pagina, name='bebida'),
     url(r'^admin/', include(admin.site.urls)),
 )
