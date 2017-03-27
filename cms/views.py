0c25d32d47d1abc4395520d9f00b6ac904d2db70
Pablo Pavon Pavon <ppavon@alpha26.aulas.gsyc.urjc.es>
2017-03-27 15:50:35 +0200

    Views modificado

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from cms.models import Pages
 # Create your views here.
+
+def barra(request):
+    respuesta = "Esto es la barra,"
+    respuesta += " y esto es lo que hay:"
+    lista = Pages.objects.all()
+    for pagina in lista:
+        respuesta += '<br><a href="/page/' + str(pagina.id) + '">' + pagina.name + '</a>'
+
+    return HttpResponse(respuesta)
+
+def pagina(request,identificador):
+    try:
+        pagina = Pages.objects.get(id=identificador)
+        respuesta = "Has introducido " + pagina.name + " que corresponde a: " + str(pagina.page) + " y el id es: " + str(pagina.id)
+    except Pages.DoesNotExist:
+        respuesta = "No existe la pagina con el identificador " + str(identificador)
+
+    return HttpResponse(respuesta)
